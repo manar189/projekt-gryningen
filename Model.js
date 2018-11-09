46bb5b108fc76eb9deda8fb26d58968957ba820b
marha690 <44838290+marha690@users.noreply.github.com>
2018-11-09 10:29:03 +0100

    Add files via upload

---
+++ Model.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Model extends Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+  render() {
+
+    return (
+      <div>
+          <h1> Inside Model.js </h1>
+      </div>
+    );
+  }
+}
+
+export default Model;
---
+++ Result.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Result extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+
+  render() {
+
+    return (
+      <div>
+          <h1> Inside Result.js </h1>
+      </div>
+    );
+  }
+}
+
+export default Result;
