28823aef08b77f8770ea26790c4bc2aa57266a9c
darkdoof <34091677+darkdoof@users.noreply.github.com>
2018-11-14 13:56:10 +0100

    Add files via upload

---
+++ Model.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Model extends Component {
+
+  constructor(props){
+    super(props)
+
+  }
+
+  myFunc(item){
+    
+  }
+
+  render() {
+
+    return (
+      <div>
+          <h1> Inside Model.js </h1>
+      </div>
+    );
+  }
+}
+
+export default Model;
---
+++ Stripes.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class Stripes extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+
+  render() {
+
+    return (
+      <div>
+          <h1> Inside Stripes.js </h1>
+      </div>
+    );
+  }
+}
+
+export default Stripes;
