dde4acdcf507a3ad10e1cf374498af89a47a373d
marha690 <44838290+marha690@users.noreply.github.com>
2018-11-21 22:14:53 +0100

    Databassökningen klar
    fixat all komunikation mellan Databas.json och javascript filerna så nu borde det bara vara att fixa utseende och visa allt som vi vill.

--- src/Result.js
+++ src/Result.js
@@ -6,14 +6,19 @@ class Result extends Component {
 
   constructor(props){
     super(props)
+
   }
 
 
   render() {
+    const data = this.props.data; //vår valda ovve
+
 
     return (
       <div>
-          <h1> Inside Result.js </h1>
+        <h1> Resulat </h1>
+
+        <h1> {data.prog} </h1>
       </div>
     );
   }
--- src/Stripes.js
+++ src/Stripes.js
@@ -1,52 +1,84 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import OvveData from './Databas'
+import Databas from './Databas'
 
 class Stripes extends Component {
 
+
   constructor(props){
     super(props)
-
-    const chosenColor = this.props.color;
-    const chosenModel = this.props.model;
     this.nextPage = this.nextPage.bind(this) //Måste vara med om funktionen anropas i render().
-
-    let counter = 0;
-    let currentOvve;
-
-    function checkStripes(ovve)
-    {
-      if(ovve.color == chosenColor && ovve.model == chosenColor)
-      {
-        ++counter;
-        currentOvve = ovve;
-      }
-    }
-/* //Behöver fixas
-    OvveData.forEach(checkStripes)
-
-    if(counter < 2)
-    {
-      this.nextPage(currentOvve);
-    }
-*/
   }
 
-  nextPage(item){
 
-   const setModel = this.props.setStripes;
-   setModel(item.revar);
- }
 
+ //retunerar true om vi måste kolla revär annars false.
+ checkStripes(){
+   const color = this.props.color;
+   const model = this.props.model;
+   let cc = 0;
+   Databas.map((item,i) => {
+
+     if(item.model == model && item.color == color){
+       cc++;
+     }
+
+   })
+   if(cc >= 2){
+      return true;
+    }
+    else {
+      return false;
+    }
+
+}
+
+nextPage(item){
+ const setData = this.props.setData;
+ setData(item);
+}
+
+//skippar sidan om vi inte behöver kolla revär.
+  componentWillMount(){
+    const color = this.props.color;
+    const model = this.props.model;
+
+    if(this.checkStripes() == false){
+
+      Databas.map((item,i) => {
+        if(item.model == model && item.color == color){
+          this.nextPage(item)
+        }
+      })
+    }
+
+  }
 
 
 
   render() {
-
+    const color = this.props.color;
+    const model = this.props.model;
     return (
       <div>
-          <h1> Inside Stripes.js </h1>
+        <h1> inside stripes </h1>
+          <div>
+            {Databas.map((item, i) =>{
+
+              if(item.model == model && item.color == color){
+                return (
+                  <div style={{padding: '5px'}}>
+
+                    <div onClick={(e) => this.nextPage(item,e)}  style={{backgroundColor: item.revar[1] }}>{item.revar[1]}</div>
+                    <div onClick={(e) => this.nextPage(item,e)}  style={{backgroundColor: item.revar[2] }}>{item.revar[1]}</div>
+                    <div onClick={(e) => this.nextPage(item,e)}  style={{backgroundColor: item.revar[3] }}>{item.revar[1]}</div>
+
+                  </div>
+                )
+              }
+
+            })}
+          </div>
       </div>
     );
   }
