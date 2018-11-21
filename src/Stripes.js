260283d246947a93daa743ec50318f8b92155be9
Måns <mkjaronsson@gmail.com>
2018-11-21 17:15:39 +0100

    Så stripes inte crashar

--- src/Stripes.js
+++ src/Stripes.js
@@ -6,6 +6,27 @@ class Stripes extends Component {
 
   constructor(props){
     super(props)
+
+    const chosenColor = this.props.color;
+    const chosenModel = this.props.model;
+    this.nextPage = this.nextPage.bind(this) //Måste vara med om funktionen anropas i render().
+
+    let counter = 0;
+    let currentOvve;
+
+    function checkStripes(ovve)
+    {
+      if(ovve.color == chosenColor && ovve.model == chosenColor)
+      {
+        ++counter;
+        currentOvve = ovve;
+      }
+    }
+
+    if(counter < 2)
+    {
+      this.nextPage(currentOvve);
+    }
   }
 
 
