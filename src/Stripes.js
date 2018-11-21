294ead7a295fe87b0a47174f5421e4914eaf4fe1
Måns <mkjaronsson@gmail.com>
2018-11-21 16:43:50 +0100

    Nu då!

--- src/Stripes.js
+++ src/Stripes.js
@@ -6,6 +6,36 @@ class Stripes extends Component {
 
   constructor(props){
     super(props)
+
+    const chosenColor = this.props.color;
+    const chosenModel = this.props.model;
+    this.nextPage = this.nextPage.bind(this) //Måste vara med om funktionen anropas i render().
+
+    let counter = 0;
+    let currentOvve;
+
+    function checkStripes(ovve)
+    {
+      if(ovve.color == chosenColor && ovve.model == chosenColor)
+      {
+        counter
+      }
+      currentOvve = ovve;
+    }
+
+    OvveData.forEach(checkModel)
+    {
+      if(hasOvve == true && hasHang == true)
+      {
+        skipPage = false;
+      }
+    }
+
+    if(skipPage)
+    {
+      this.nextPage(currentOvve);
+    }
+  }
   }
 
 
